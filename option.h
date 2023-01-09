3f136ac479ddad64511a26d83cd3579b9192c1b1
marcarro <A00837236@tec.mx>
2023-01-09 16:22:40 -0600

    changed option struct to class

--- option.h
+++ option.h
@@ -1,11 +1,23 @@
 #pragma once
 
-struct option {
-  double price;   // asset price
-  double strike;  // strike price
-  double vol;     // volatility
-  double r;       // risk-free interest rate
-  double div;     // dividend yield
-  double T;       // option maturity
-  char type;      // option type
+class Option {
+private:
+  double _price;   // asset price
+  double _strike;  // strike price
+  double _vol;     // volatility
+  double _rate;    // risk-free interest rate
+  double _div;     // dividend yield
+  double _T;       // option maturity
+  char _type;      // option type
+public:
+  Option(double price, double strike, double vol, double rate, double div, double T, char type):
+  _price  { price },
+  _strike { strike },
+  _vol    { vol },
+  _rate   { rate },
+  _div    { div },
+  _T      { T },
+  _type   { type }
+  {}
+  double black_scholes();
 };
